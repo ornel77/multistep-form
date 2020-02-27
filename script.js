4eca06ba39f5f3d1e92c717bb3d0df1b5b2b4a41
ornel77 <t.ornel@yahoo.fr>
2020-02-27 11:52:04 +0100

    prev function broke the website to be continued

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const prev = document.querySelector('.prev')
 const next = document.querySelector('.next')
 const stepNumbers = document.querySelectorAll('.step-number')
-const tabs = document.querySelectorAll('.tabs')
+const tabs = document.querySelectorAll('[data-tabs]')
 const steps = document.querySelectorAll('[data-number]')
 
 /* Convert node list into arrays*/
@@ -10,11 +10,13 @@ const stepNumbersArr = [].slice.call(stepNumbers)
 
 /* ---------------------------- FUNCTION HANDLER ---------------------------- */
 let index = 0
+let tabIndex = 0
 
 /* NEXT STEP FUNCTION */
 const nextStep = (e) => {
     e.preventDefault()
     
+    //NUMBER
     if(steps[index].classList.contains('active')) {
         steps[index].classList.remove('active')
         steps[index+1].classList.add('active')
@@ -28,29 +30,42 @@ const nextStep = (e) => {
         next.style.cursor= 'initial'
     }
 
-    // tabs.forEach((tab, index) => {
-    //     console.log(index);
-    //     if(tab.classList.contains('invisible')) {
-    //         tab.classList.remove('invisible')
-    //     } else {
-    //         tab.className += ' invisible'
-    //     }
-        
-    // })
+    //TAB
+    if(tabs[tabIndex].classList.contains('visible')) {
+        console.log('hello');
+        tabs[tabIndex].classList.remove('visible')
+        tabs[tabIndex+1].classList.add('visible')
+        tabIndex++
+    }
 }
 /* PREV STEP FUNCTION */
+let prevNumIndex = 0
+let prevTabIndex = 0
 const prevStep = e => {
     e.preventDefault()
+    //NUMBER
+    if(steps[index].classList.contains('active')) {
+        steps[index].classList.remove('active')
+        steps[index-1].classList.add('active')
+        prev.style.display = 'block'
+        index--
+    }
+
+    let firstElmt = steps[0]
+    if(steps[index] === firstElmt) {
+        // next.disabled = false
+        // next.style.backgroundColor = 'initial'
+        // next.style.cursor= 'pointer'
+
+        prev.style.display = 'none'
+    }
 }
  
 /* ----------------------------- EVENT LISTENER ----------------------------- */
 /* ONLOAD */
 window.onload = () => {
-    for (let i = 1; i < tabs.length; i++) {
-        tabs[i].className += ' invisible'
-    }
-
-    stepNumbers[0].className += ' active'
+    steps[0].className += ' active'
+    tabs[0].className += ' visible'
 
     prev.style.display = 'none'
 
